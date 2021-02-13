2138a78ba0e02aa573537ea12ba8f79c77f9c860
Cris Franco <cris.franco219@gmail.com>
2021-02-12 21:16:06 -0500

    finished routes and exported router

--- controllers/burgers_controller.js
+++ controllers/burgers_controller.js
@@ -13,3 +13,22 @@ router.get("/", function (req, res) {
         res.render("index", hbsObject);
     });
 });
+
+router.post("/", function (req, res) {
+    burger.insertOne("burger_name",
+        req.body.name, function () {
+            res.redirect("/");
+        });
+});
+
+router.put("/:id", function (req, res) {
+    var condition = "id = " + req.params.id;
+
+    burger.updateOne({
+        devoured: true
+    }, { id: req.params.id }, function () {
+        res.redirect("/");
+    });
+});
+
+module.exports = router;
